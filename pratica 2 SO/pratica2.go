64e6fe3b2cbe3031e2ab9b62467b9e0277fdaa11
Rahmon J <rahmonjorge1981@gmail.com>
2022-07-29 09:33:18 -0300

    first commit

---
+++ pratica 2 SO/pratica2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/* HOW TO USE
+1. rename this to main and rename introduction.main to main2 to run.
+2. uncomment each function call to see examples of concurrency.
+*/
+//
+func main() {
+	resource1 := make chan(int)
+	resource2 := make chan(int)
+}
