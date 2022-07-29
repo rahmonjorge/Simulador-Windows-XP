8555a747e5bfc7f3a94a9a0d3231e4b9983ab106
Rahmon J <rahmonjorge1981@gmail.com>
2022-07-29 19:38:46 -0300

    esqueleto da questao 1

--- pratica 2 SO/pratica2.go
+++ pratica 2 SO/pratica2.go
@@ -6,12 +6,40 @@ import (
 	"time"
 )
 
-/* HOW TO USE
-1. rename this to main and rename introduction.main to main2 to run.
-2. uncomment each function call to see examples of concurrency.
-*/
-//
+var global1 int
+var global2 int
+
+var (
+	lock sync.Mutex
+)
+
 func main() {
-	resource1 := make chan(int)
-	resource2 := make chan(int)
+	// Init variables
+	global1 = 0
+	iterations := 5
+
+	// Wait Group
+	var wg sync.WaitGroup
+	wg.Add(iterations)
+
+	// Threads
+	go increment("[A]", &wg)
+	go increment("[B]", &wg)
+	go increment("[C]", &wg)
+	go increment("[D]", &wg)
+	go increment("[E]", &wg)
+
+	// Wait
+	wg.Wait()
+}
+
+func increment(name string, wg *sync.WaitGroup) {
+	start := time.Now()
+	fmt.Println(name, "Reading globals as: ", global1, global2)
+	global1 = global1 + 1
+	global2 = global2 + 1
+	fmt.Println(name, "I changed global's value. now they are: ", global1, global2)
+	wg.Done()
+	end := time.Now()
+	fmt.Println(name, "Time of this thread's execution: ", time.Time.Sub(end, start))
 }
