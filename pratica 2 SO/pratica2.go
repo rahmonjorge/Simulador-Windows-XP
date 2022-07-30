e10b8bca47bd31fed38501fc1358670d2d64f423
Rahmon J <rahmonjorge1981@gmail.com>
2022-07-30 11:28:14 -0300

    textos coloridos

--- pratica 2 SO/pratica2.go
+++ pratica 2 SO/pratica2.go
@@ -7,15 +7,24 @@ import (
 	"time"
 )
 
-var global1 int
-var global2 int
+var global1 int = 0
+var global2 int = 0
 var mut1 sync.Mutex
 var mut2 sync.Mutex
 
+var colorReset string = "\033[0m"
+
 func main() {
+
+	//colorRed := "\033[31m"
+	colorGreen := "\033[32m"
+	colorYellow := "\033[33m"
+	colorBlue := "\033[34m"
+	colorPurple := "\033[35m"
+	//colorCyan := "\033[36m"
+	colorWhite := "\033[37m"
+
 	// Init variables
-	global1 = 0
-	global2 = 0
 	iterations := 5
 
 	// Wait Group
@@ -23,60 +32,61 @@ func main() {
 	wg.Add(iterations)
 
 	// Threads
-	go increment("[A]", &wg)
-	go increment("[B]", &wg)
-	go increment("[C]", &wg)
-	go increment("[D]", &wg)
-	go increment("[E]", &wg)
+	go increment("[A]", colorWhite, &wg)
+	go increment("[B]", colorGreen, &wg)
+	go increment("[C]", colorYellow, &wg)
+	go increment("[D]", colorBlue, &wg)
+	go increment("[E]", colorPurple, &wg)
 
 	// Wait
 	wg.Wait()
 }
 
-func increment(name string, wg *sync.WaitGroup) {
+func increment(name string, color string, wg *sync.WaitGroup) {
 	start := time.Now()
 	s1 := rand.NewSource(start.UnixNano())
 	r1 := rand.New(s1)
 	loop := 0
-	for loop < 25 {
+	for loop < 20 {
 		choice := r1.Intn(2) + 1
-		fmt.Printf("%s %s%d\n", name, "Trying to read the global", choice)
+		//fmt.Printf("%s %s%d\n", name, "Trying to read the global", choice)
+		fmt.Println(color, name, "Trying to read the global", choice, colorReset)
 		switch choice {
 		case 1:
 			if mut1.TryLock() {
-				fmt.Println(name, "Reading global1 as: ", global1)
-				fmt.Println(mut1)
+				fmt.Println(color, name, "Reading global1 as: ", global1, colorReset)
+				fmt.Println(color, mut1, colorReset)
 			} else {
-				fmt.Println(name, "Blocked, awaiting access to global1...")
+				fmt.Println(color, name, "Blocked, awaiting access to global1...", colorReset)
 				mut1.Lock()
-				fmt.Println(name, "Reading global1 as: ", global1)
+				fmt.Println(color, name, "Reading global1 as: ", global1, colorReset)
 			}
 			for i := 0; i < 5; i++ {
 				global1 += 1
 				time.Sleep(10 * time.Millisecond)
 			}
-			fmt.Println(name, "I changed global1 value. Now it is: ", global1)
+			fmt.Println(color, name, "I changed global1 value. Now it is: ", global1, colorReset)
 			mut1.Unlock()
 
 		case 2:
 			if mut2.TryLock() {
-				fmt.Println(name, "Reading global2 as: ", global2)
-				fmt.Println(mut2)
+				fmt.Println(color, name, "Reading global2 as: ", global2, colorReset)
+				fmt.Println(color, mut2, colorReset)
 			} else {
-				fmt.Println(name, "Blocked, awaiting access to global2...")
+				fmt.Println(color, name, "Blocked, awaiting access to global2...", colorReset)
 				mut2.Lock()
-				fmt.Println(name, "Reading global2 as: ", global2)
+				fmt.Println(color, name, "Reading global2 as: ", global2, colorReset)
 			}
 			for i := 0; i < 5; i++ {
 				global2 += 1
 				time.Sleep(100 * time.Millisecond)
 			}
-			fmt.Println(name, "I changed global2 value. Now it is: ", global2)
+			fmt.Println(color, name, "I changed global2 value. Now it is: ", global2, colorReset)
 			mut2.Unlock()
 		}
 		loop += 1
 	}
 	end := time.Now()
-	fmt.Println(name, "Time of this thread's execution: ", time.Time.Sub(end, start))
+	fmt.Println(color, name, "Time of this thread's execution: ", time.Time.Sub(end, start), colorReset)
 	wg.Done()
 }
