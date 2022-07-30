ab5be8a8d28d2366462a6625935f31ca60ef6af5
Rahmon J <rahmonjorge1981@gmail.com>
2022-07-30 13:10:20 -0300

    esqueleto da questao2-simulador

---
+++ pratica 2 SO/questao2-simulador/Program.cs
@@ -0,0 +1,29 @@
+﻿class Process
+{
+    int id;
+    string name;
+    int priority;
+    bool ioBound;
+    bool cpuBound;
+    int cpuTime; // Em ms
+
+    public Process(int id, string name, int priority, bool ioBound, bool cpuBound, int cpuTime)
+    {
+        this.id = id;
+        this.name = name;
+        this.priority = priority;
+        this.ioBound = ioBound;
+        this.cpuBound = cpuBound;
+        this.cpuTime = cpuTime; // Em ms
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        List<Process> processes = new List<Process>();
+    }
+}
